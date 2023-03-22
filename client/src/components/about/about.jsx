81ebc0dd3ccb1946621c935631c2da1c6cedce83
RameesAP <rameeswork@gmail.com>
2023-03-23 02:48:26 +0530

    about succes

--- client/src/components/about/about.jsx
+++ client/src/components/about/about.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/profile.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Exprtience</h5>
+              <small>1 Year Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>20+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>20+ Completed</small>
+            </article>
+
+          </div>
+          <p>
+            A Self-taught MERN Stack developer who is in the process of
+            learning the newest technologies and adapting with it, excited
+            to work on challenging projects which requires greater
+            problem-solving skills.
+
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+
+        </div>
+      </div>
+
+    </section>
   )
 }
 
