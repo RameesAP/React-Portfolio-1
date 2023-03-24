ac125f23adc2f2d5da971ed1227594b8b40d4fea
RameesAP <rameeswork@gmail.com>
2023-03-25 03:00:35 +0530

    pagination

--- client/src/components/testimonials/testimonials.jsx
+++ client/src/components/testimonials/testimonials.jsx
@@ -5,65 +5,78 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Ashwanth',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptatem, quidem quisquam distinctio eos earum ducimus laboriosam obcaecati? Earum hic dolor nemo."
+  },
+  {
+    avatar: AVTR2,
+    name: 'Anoob',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptatem, quidem quisquam distinctio eos earum ducimus laboriosam obcaecati? Earum hic dolor nemo."
+  },
+  {
+    avatar: AVTR3,
+    name: 'Nandhu',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptatem, quidem quisquam distinctio eos earum ducimus laboriosam obcaecati? Earum hic dolor nemo."
+  },
+  {
+    avatar: AVTR4,
+    name: 'Javad',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptatem, quidem quisquam distinctio eos earum ducimus laboriosam obcaecati? Earum hic dolor nemo."
+  },
+
+]
+
 const testimonials = () => {
   return (
     <section id='testimonials'>
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className='container testimonials__container'>
-        <article className='testimonial'>
-          <div className='client__avatar'>
-            <img src={AVTR1} alt="Avatar One" />
-          </div>
-          <h5 className='client__name'>Ashwanth</h5>
-          <small className='client__review'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Voluptatem, quidem quisquam distinctio eos earum ducimus laboriosam obcaecati? Earum hic dolor nemo.
-            Incidunt voluptatibus corrupti
-            accusantium exercitationem rerum molestias est assumenda.
-          </small>
-        </article>
+      <Swiper className='container testimonials__container' 
+        // install Swiper modules
+        modules={[ Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        
+        pagination={{ clickable: true }}
+       >
 
-        <article className='testimonial'>
-          <div className='client__avatar'>
-            <img src={AVTR1} alt="Avatar One" />
-          </div>
-          <h5 className='client__name'>Ashwanth</h5>
-          <small className='client__review'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Voluptatem, quidem quisquam distinctio eos earum ducimus laboriosam obcaecati? Earum hic dolor nemo.
-            Incidunt voluptatibus corrupti
-            accusantium exercitationem rerum molestias est assumenda.
-          </small>
-        </article>
+        {
+          data.map(({ avatar, name, review }, index) => {
+            return (
+              <SwiperSlide key={index} className='testimonial'>
+                <div className='client__avatar'>
+                  <img src={avatar} />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>
+                  {review}
+                </small>
+                </SwiperSlide>
+            )
+          })
+        }
 
-        <article className='testimonial'>
-          <div className='client__avatar'>
-            <img src={AVTR1} alt="Avatar One" />
-          </div>
-          <h5 className='client__name'>Ashwanth</h5>
-          <small className='client__review'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Voluptatem, quidem quisquam distinctio eos earum ducimus laboriosam obcaecati? Earum hic dolor nemo.
-            Incidunt voluptatibus corrupti
-            accusantium exercitationem rerum molestias est assumenda.
-          </small>
-        </article>
 
-        <article className='testimonial'>
-          <div className='client__avatar'>
-            <img src={AVTR1} alt="Avatar One" />
-          </div>
-          <h5 className='client__name'>Ashwanth</h5>
-          <small className='client__review'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Voluptatem, quidem quisquam distinctio eos earum ducimus laboriosam obcaecati? Earum hic dolor nemo.
-            Incidunt voluptatibus corrupti
-            accusantium exercitationem rerum molestias est assumenda.
-          </small>
-        </article>
-      </div>
+
+
+
+
+      </Swiper>
     </section>
   )
 }
