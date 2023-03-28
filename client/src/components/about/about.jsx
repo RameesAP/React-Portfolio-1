d9d8b10bd3cf2390cb99a9ba1f6ced8d5b178a08
RameesAP <rameeswork@gmail.com>
2023-03-28 14:04:02 +0530

    ex

--- client/src/components/about/about.jsx
+++ client/src/components/about/about.jsx
@@ -29,7 +29,7 @@ const About = () => {
             <article className='about__card'>
               <FiUsers className='about__icon' />
               <h5>Clients</h5>
-              <small>20+ Worldwide</small>
+              <small>0 Worldwide</small>
             </article>
 
             <article className='about__card'>
