a9294c52671c0873904ba4923934bae302064fce
RameesAP <rameeswork@gmail.com>
2023-04-06 15:45:09 +0530

    serv

--- client/src/components/services/services.jsx
+++ client/src/components/services/services.jsx
@@ -16,24 +16,24 @@ const services = () => {
           <ul className='service__list'>
             <li>
               <BiCheck className='service__list-icon' />
-              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p> Create user-friendly and aesthetically pleasing interfaces for web and mobile applications.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p> Using Figma: Design and prototype interactive features and animations using Figma's powerful tools and plugins.</p>
             </li>
-            <li>
+            {/* <li>
               <BiCheck className='service__list-icon' />
               <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </li>
-            <li>
+            </li> */}
+            {/* <li>
               <BiCheck className='service__list-icon' />
               <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </li>
-            <li>
+            </li> */}
+            {/* <li>
               <BiCheck className='service__list-icon' />
               <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </li>
+            </li> */}
           </ul>
         </article>
 
@@ -41,33 +41,33 @@ const services = () => {
 
         <article className='service'>
           <div className="service__head">
-            <h3>Web Development</h3>
+            <h3>Front-End</h3>
           </div>
           <ul className='service__list'>
             <li>
               <BiCheck className='service__list-icon' />
-              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p> Implemented front end features such as authentication, routing, state management, and data visualization using React.js and various libraries</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p> Integrated RESTful APIs and web services to communicate with back end server and database using Express.js and Node.js</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p> Utilized MongoDB as a NoSQL database to store and retrieve data in JSON format</p>
             </li>
-            <li>
+            {/* <li>
               <BiCheck className='service__list-icon' />
               <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </li>
-            <li>
+            </li> */}
+            {/* <li>
               <BiCheck className='service__list-icon' />
               <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </li>
-            <li>
+            </li> */}
+            {/* <li>
               <BiCheck className='service__list-icon' />
               <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </li>
+            </li> */}
           </ul>
         </article>
 
@@ -76,29 +76,29 @@ const services = () => {
 
         <article className='service'>
           <div className="service__head">
-            <h3>Content Creation</h3>
+            <h3>Back-End</h3>
           </div>
           <ul className='service__list'>
             <li>
               <BiCheck className='service__list-icon' />
-              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p> Developed and deployed RESTful APIs using Node.js, Express.js and MongoDB for various web applications</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p> Implemented authentication, authorization and data validation using JWT, bcrypt and mongoose</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p> Utilized cloud services such as AWS, Heroku and MongoDB Atlas to host and scale the back end services</p>
             </li>
-            <li>
+            {/* <li>
               <BiCheck className='service__list-icon' />
               <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </li>
-            <li>
+            </li> */}
+            {/* <li>
               <BiCheck className='service__list-icon' />
               <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </li>
+            </li> */}
           </ul>
         </article>
 
